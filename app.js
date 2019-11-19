5c8284796af4dc007ed12579abdf56a12162cb0d
khalilcodes <khalilali.j@gmail.com>
2019-11-19 17:47:09 +0530

    0.01

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
 require('./dbconnect')
 
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(cookieParser())
+
+app.get('/', (req,res)=> {
+    res.send("homepage /")
+})
 
 app.listen(port, ()=> {
     console.log(`server started at ${port}`)
